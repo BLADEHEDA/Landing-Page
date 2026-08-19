b3b7ecf46ce5f881af348754783bed7b65c33468
BLADEHEDA <nkwetakembrunodud@gmail.com>
2026-08-19 09:11:08 +0100

    feature mobile responsivenes main page done

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-
 import HeaderSection from "./components/HeaderSection";
 import Hero from "./components/Hero";
 import HomePage from "./components/HomePage";
@@ -6,12 +5,9 @@ import Section1 from "./components/Section1";
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-           <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-18 px-16 bg-blac dark:bg-black sm:items-start">
-       <HomePage/>  
-    <div>
-     
-    </div>
+    <div className="flex flex-col min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <main className="flex flex-col w-full w-[95%] sm:w-[85%] md:w-[80%] lg:max-w-2xl xl:max-w-3xl items-center justify-between py-6 sm:py-10 md:py-12 px-3 sm:px-6 md:px-8 bg-white dark:bg-black transition-all mx-auto">
+        <HomePage />
       </main>
     </div>
   );
